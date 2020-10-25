9ab7f531a7540237530d47592f2592ea1a79f294
Jonathan Demirgian <trydemirgian@gmail.com>
2020-10-25 16:10:18 -0400

    comment

--- foo_pkg/__init__.py
+++ foo_pkg/__init__.py
@@ -1,1 +1,3 @@
+# This import will prevent the test patch from resolving the desired target in foo_pkg/foo.
+# It will only find the function here imported as foo, not the module foo.
 from .foo import foo
--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,8 @@
 from unittest.mock import patch
 
 
+# The patch errors with
+# E AttributeError: <function foo at 0x10552b430> does not have the attribute 'bar'
 @patch('foo_pkg.foo.bar')
 def test():
     assert True
